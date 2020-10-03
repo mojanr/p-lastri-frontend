a71295a9c958ed3438b1095a6567dbd97511bcca
Mochammad Fauzan Rizqulloh <mochammadfauzanrizqulloh@gmail.com>
2020-10-03 12:42:51 +0700

    upload build
    upload build

---
+++ build/precache-manifest.63c196b8e791997bb41ad5d7e2743f84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850888738182b32b6c28bb10395c7902",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eac1a8517a9f6ff14e0d",
+    "url": "/static/css/2.72cd66f8.chunk.css"
+  },
+  {
+    "revision": "b4b787e6573a6f031cbc",
+    "url": "/static/css/main.52c55ada.chunk.css"
+  },
+  {
+    "revision": "eac1a8517a9f6ff14e0d",
+    "url": "/static/js/2.1203ec6b.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/static/js/2.1203ec6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b787e6573a6f031cbc",
+    "url": "/static/js/main.a6434d1b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
